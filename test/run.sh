d14dfbd6f2736f153d8cb5065acee08ca73f939a
junhangc <junhangc@moqi.ai>
2024-10-15 15:20:58 +0800

    model: code on nndeploy

--- test/run.sh
+++ test/run.sh
@@ -1,11 +1,13 @@
 set -x
 abspath="$(realpath $0)"
 basepath=${abspath%/*}
-$basepath/../build/nndeploy_demo_detect --name NNDEPLOY_YOLOV8 --inference_type kInferenceTypeOnnxRuntime \
+$basepath/../build/clfg --name clfg --inference_type kInferenceTypeOnnxRuntime \
 --device_type kDeviceTypeCodeX86:0 --model_type kModelTypeOnnx --is_path \
---model_value /Users/junhangc/sideproject/nndeploy/test/yolov8n.onnx \
+--model_value  \
+/Users/junhangc/sideproject/stream_pipe/python/models/2403_cl_fg/assets/k2_fg_exp6_6-300.onnx \
 --codec_flag kCodecFlagImage --parallel_type kParallelTypeSequential \
---input_path /Users/junhangc/sideproject/stream_pipe/python/models/m2409_yolov8/data/bus.jpg \
+--input_path \
+/Users/junhangc/sideproject/stream_pipe/python/models/2403_cl_fg/assets/11_unroll.png \
 --output_path /tmp/sample_output.jpg
 
 # --input_path /Users/junhangc/sideproject/nndeploy/test/test_data_detect_sample.jpg \
